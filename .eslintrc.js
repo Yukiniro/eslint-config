11be886697ca96c71ee555de34846c38594229f6
Zhanghao <yukiniro@hotmail.com>
2022-03-20 16:07:03 +0800

    feat: update .eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["standard", "plugin:@typescript-eslint/recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
